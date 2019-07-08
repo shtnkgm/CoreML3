236b26c35632e6e0e716e033ffa78e43093a8b9b
shtnkgm <shtnkgm@gmail.com>
2019-07-08 23:25:41 +0900

    Initial Commit

---
+++ CoreML3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreML3
+//
+//  Created by Shota Nakagami on 2019/07/08.
+//  Copyright © 2019 Shota Nakagami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
